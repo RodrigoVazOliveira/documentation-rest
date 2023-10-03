4756630e65bfcfcb9794f3c4a123b6b961214f0a
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2023-10-03 19:31:52 -0300

    Initial project

---
+++ src/main/java/dev/rodrigovaz/api/ApiRestDocumentApplication.java
@@ -0,0 +1,13 @@
+package dev.rodrigovaz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDocumentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rodrigovaz/api/ApiRestDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rodrigovaz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
